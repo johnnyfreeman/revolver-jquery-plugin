d7484a289407442273dcec9c1ab8811f00fd5f69
Johnny Freeman <prsjohnny@gmail.com>
2014-06-02 15:16:12 -0400

    added instance method
    added a convenience (instance) method that wraps dom elements (that are within the container element) with jquery object

--- revolver.js
+++ revolver.js
@@ -3,7 +3,7 @@
   // make revolver use Sizzle instead of querySelectorAll
   Revolver.setSelectorEngine($.find);
 
-  // plugin
+  // jquery plugin
   $.fn.revolver = function(options) {
     return this.each(function () {
       // store the revolver object using jquery's data method
@@ -13,6 +13,12 @@
         $.data(this, 'revolver', new Revolver(options));
       }
     });
+  };
+  
+  // convenience method that wraps dom elements (that are 
+  // within the container element) with jquery object 
+  Revolver.prototype.$ = function (selector) {
+    return $(selector, this.container);
   };
 
 }(jQuery));
